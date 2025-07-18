6a63672fabc26b877c9aa4e23fbdad2410f37b5b
Garza-Dev <egarzajr.00@gmail.com>
2025-07-18 15:56:27 -0500

    Initial Commit

---
+++ BeanBook/BeanBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanBookApp.swift
+//  BeanBook
+//
+//  Created by Eduardo Garza on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BeanBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
